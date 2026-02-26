bf2692d80b679d5a099747a0a46b446af1eb0088
Sawyer <sawyer.c.theis@gmail.com>
2026-02-26 13:25:46 -0600

    remove back wheel encoder

--- Robot/RobotContainer.py
+++ Robot/RobotContainer.py
@@ -20,7 +20,7 @@ from Robot.Commands.FollowPathCmd import FollowPathCmd
 class RobotContainer:
     def __init__(self):
         self.uwb = UWB()
-        self.back_Wheel_encoder = BackWheelEncoder()
+        #self.back_Wheel_encoder = BackWheelEncoder()
         self.imu = IMU()
         self.clutches = Clutches()
         self.path_following = PathFollowing()
@@ -29,7 +29,7 @@ class RobotContainer:
          # Start subsystems
         self.clutches.start(left_clutch_pin=Constants.left_clutch_pin, right_clutch_pin=Constants.right_clutch_pin)
         self.uwb.start(uwb_tag_data=Constants.uwb_tag_data, anchors_pos=None)
-        self.back_Wheel_encoder.start(pin=Constants.back_right_encoder_pin, active_high=True, pull_up=True, debounce_ms=10, edge='falling', wheel_circumference=Constants.wheel_circumference, counts_per_revolution=Constants.counts_per_revolution)
+        # self.back_Wheel_encoder.start(pin=Constants.back_right_encoder_pin, active_high=True, pull_up=True, debounce_ms=10, edge='falling', wheel_circumference=Constants.wheel_circumference, counts_per_revolution=Constants.counts_per_revolution)
         
         # self.motor_control.default_command(MiniBullseyeControlCmd(self.motor_control, self.path_following))
         #self.path_following.default_command(FollowPathCmd(self.motor_control, self.path_following))
@@ -42,7 +42,7 @@ class RobotContainer:
         # AlignIMUToWorldCmd(tau=0.5, duration=30.0).schedule()
                 
     def shutdown(self):
-        self.back_Wheel_encoder.stop()
+        # self.back_Wheel_encoder.stop()
         self.clutches.stop()
         self.uwb.stop_all()
         self.motor_control.stop()
