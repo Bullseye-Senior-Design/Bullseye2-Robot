998969e6de356ab58c49f521a470b865d04311f2
Max <58192471+DiMister@users.noreply.github.com>
2026-02-26 17:08:28 -0600

    occlate speed

--- Robot/Commands/MotorMovementExampleCmd.py
+++ Robot/Commands/MotorMovementExampleCmd.py
@@ -2,6 +2,8 @@ from Robot.subsystems.HeaderHealerSwitches import HeaderHealerSwitches
 from Robot.subsystems.Clutches import Clutches
 from Robot.subsystems.DriveTrain import DriveTrain
 from structure.commands.Command import Command
+import time
+import math
 import logging
 
 logger = logging.getLogger("MotorMovementExampleCmd")
@@ -19,12 +21,16 @@ class MotorMovementExampleCmd(Command):
         self.add_requirement(self.clutches)
         
     def initialize(self):
+        self.start_time = time.time()
         self.speed = 1
         
             
         pass
     
     def execute(self):
+        elapsed_time = time.time() - self.start_time
+        # Cycle between -1 and 1 with a period of 10 seconds
+        self.speed = math.sin(elapsed_time * 2 * math.pi / 10.0)
         self.drive_train.set_speed_angle(self.speed, 180)
 
         if self.header_healer_switches.get_header_switch_triggered():
@@ -33,7 +39,6 @@ class MotorMovementExampleCmd(Command):
             self.clutches.engage_right_clutch()
             self.drive_train.disengage_backwheel()
             self.drive_train.disengage_frontwheel()
-            self.speed = -1
             
         if self.header_healer_switches.get_healer_switch_triggered():
             logger.debug("Healer switch triggered: Disengaging clutches and engaging wheels")
@@ -41,7 +46,6 @@ class MotorMovementExampleCmd(Command):
             self.clutches.disengage_right_clutch()
             self.drive_train.engage_backwheel()
             self.drive_train.engage_frontwheel()
-            self.speed = 1
         pass
     
     def end(self, interrupted):
