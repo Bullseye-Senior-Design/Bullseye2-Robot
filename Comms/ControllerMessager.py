5b89aca38216f8b9b2a4e0728bbcac8ec8248108
Michael_Paulsen <Michael.Paulsen@jacks.sdstate.edu>
2026-03-06 14:36:52 -0600

    lost directory

--- Comms/ControllerMessager.py
+++ Comms/ControllerMessager.py
@@ -3,6 +3,10 @@ import serial
 import time
 import pygame
 import sys
+import os
+
+# Add the parent directory to the Python path so Comms module can be found
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from Comms.JoystickData import JoystickData
 from Comms.StateData import State
