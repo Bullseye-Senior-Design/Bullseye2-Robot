3cee5b14059dd98ee972e262a9756da3c94b5f9d
Max <maximus.theis@gmail.com>
2026-03-31 17:57:58 -0500

    testing

--- Robot/Commands/MotorMovementExampleCmd.py
+++ Robot/Commands/MotorMovementExampleCmd.py
@@ -1,6 +1,7 @@
 from Robot.subsystems.HeaderHealerSwitches import HeaderHealerSwitches
 from Robot.subsystems.Clutches import Clutches
 from Robot.subsystems.DriveTrain import DriveTrain
+from Robot.subsystems.PCBLEDs import PCBLEDs
 from structure.commands.Command import Command
 import time
 import math
@@ -10,15 +11,17 @@ logger = logging.getLogger("MotorMovementExampleCmd")
 logger.setLevel(level=logging.INFO)
 
 class MotorMovementExampleCmd(Command):
-    def __init__(self, drive_train: DriveTrain, clutches: Clutches, header_healer_switches: HeaderHealerSwitches):
+    def __init__(self, drive_train: DriveTrain, clutches: Clutches, header_healer_switches: HeaderHealerSwitches, PCBLEDs: PCBLEDs):
         super().__init__()
         
         self.drive_train = drive_train
         self.clutches = clutches
         self.header_healer_switches = header_healer_switches
+        self.pcb_leds = PCBLEDs
         self.add_requirement(self.drive_train)
         self.add_requirement(self.header_healer_switches)
         self.add_requirement(self.clutches)
+        self.add_requirement(self.pcb_leds)
         
     def initialize(self):
         self.start_time = time.time()
@@ -28,15 +31,17 @@ class MotorMovementExampleCmd(Command):
         elapsed_time = time.time() - self.start_time
         # Cycle between -1 and 1 with a period of 10 seconds
         self.speed = math.sin(elapsed_time * 2 * math.pi / 10.0)
-        self.drive_train.set_speed_angle(self.speed, 90)
+        self.drive_train.set_speed_angle(self.speed, 1)
         
         #logger.debug(f"")
         #logger.debug(f"Front wheel position: {self.drive_train.get_frontwheel_position()}")
         if self.speed > 0:
+            self.pcb_leds.set_green_status_led(True)
             self.drive_train.engage_backwheel()
             self.drive_train.engage_frontwheel()
             self.clutches.engage_clutches()
         else:
+            self.pcb_leds.set_green_status_led(False)
             self.drive_train.disengage_backwheel()
             self.drive_train.disengage_frontwheel()
             self.clutches.disengage_clutches()
