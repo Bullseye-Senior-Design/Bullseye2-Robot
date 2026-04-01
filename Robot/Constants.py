348aa099d69e636c389486c9fc636a4c398cf73f
Michael_Paulsen <Michael.Paulsen@jacks.sdstate.edu>
2026-03-31 21:42:40 -0500

    random comment

--- Robot/Constants.py
+++ Robot/Constants.py
@@ -71,7 +71,7 @@ class Constants:
 
     # Controller input handling
     controller_deadzone = 0.15
-    controller_update_rate = 0.1  # seconds (20 Hz)
+    controller_update_rate = 0.1 
 
     #BMS Constants
     bms_update_interval = 0.05 # seconds between reads
