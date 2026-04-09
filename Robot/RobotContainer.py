e844527cf4554e83c49b24b8e050e4f7b8c92f32
Sawyer <sawyer.c.theis@gmail.com>
2026-04-09 17:37:11 -0500

    fix max's bug cuz he stupid

--- Robot/RobotContainer.py
+++ Robot/RobotContainer.py
@@ -105,7 +105,8 @@ class RobotContainer:
 
     def start_path_creation(self):
         logger.info("Starting path creation mode")
-        self.path_cmd = CreatePathCmd(self.path_creation, lambda: self.comm_thread.get_controller_data().btn_B).schedule()
+        self.path_cmd = CreatePathCmd(self.path_creation, lambda: self.comm_thread.get_controller_data().btn_B)
+        self.path_cmd.schedule()
     
     def disabled_init(self):
         logger.info("Robot disabled, stopping all movement")
