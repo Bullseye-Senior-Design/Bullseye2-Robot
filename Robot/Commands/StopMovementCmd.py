7c65f6234ad78ceb5264382fc26266d1e4a08152
Max <58192471+DiMister@users.noreply.github.com>
2026-04-15 19:17:12 -0500

    clutches

--- Robot/Commands/StopMovementCmd.py
+++ Robot/Commands/StopMovementCmd.py
@@ -1,13 +1,17 @@
 from Robot.subsystems.DriveTrain import DriveTrain
+from Robot.subsystems.Clutches import Clutches
 from structure.commands.Command import Command
 
 class StopMovementCmd(Command):
-    def __init__(self, drive_train: DriveTrain):
+    def __init__(self, drive_train: DriveTrain, clutches: Clutches):
         super().__init__()
         self._drive_train = drive_train
+        self._clutches = clutches
+        self.add_requirement(clutches)
         self.add_requirement(drive_train)
         
     def initialize(self):
+        self._clutches.disengage_clutches()
         self._drive_train.stop()
     
     def execute(self):
