c8b02f0d74a0b87b486da7e2648c8db977b71339
Max <maximus.theis@gmail.com>
2026-04-27 15:39:07 -0500

    new pid

--- Robot/subsystems/DriveTrain.py
+++ Robot/subsystems/DriveTrain.py
@@ -63,7 +63,7 @@ class DriveTrain(Subsystem):
             
             # Pitch PID controller
             self.front_wheel_pid = PID(
-                1.1, 0.1, 0.0, 
+                0.8, 0.1, 0.0, 
                 setpoint=0,
                 error_map=MathUtil.wrap_to_pi  # Wrap error to [-pi, pi]
             )
